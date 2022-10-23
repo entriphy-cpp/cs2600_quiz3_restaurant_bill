42c1814b41e1740935b8f0e1ba6bcef7b3d67057
Matthew Finerty <mfinerty@cpp.edu>
2022-10-23 15:08:54 -0700

    Create meal table

---
+++ mealTable.c
@@ -0,0 +1,9 @@
+#include "meal.h"
+
+Meal MealTable[] = {
+    {"Salad",     9.95},
+    {"Soup",      4.55},
+    {"Sandwich", 13.25},
+    {"Pizza",    22.35}
+};
+const int MealTableEntries = sizeof(MealTable) / sizeof(MealTable[0]);

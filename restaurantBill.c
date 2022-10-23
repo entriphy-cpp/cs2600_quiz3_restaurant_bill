da6e36f48c4481d5c607704a03923fd3c1145a6b
Matthew Finerty <mfinerty@cpp.edu>
2022-10-23 15:12:42 -0700

    Display program usage if tax and tip are not provided

--- restaurantBill.c
+++ restaurantBill.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "meal.h"
 
 // External table
@@ -5,7 +7,11 @@ extern Meal MealTable[];
 extern const int MealTableEntries;
 
 int main(int argc, char const *argv[]) {
-    // Check argument count (should be >= 3)
+    // Check argument count (should be >= 3; argv[1] = tax, argv[2] = tip)
+    if (argc < 3) {
+        printf("Usage: restaurantBill <tax> <tip>\n");
+        return EXIT_FAILURE;
+    }
 
     // Parse command line arguments for tax and tip (convert to double)
 

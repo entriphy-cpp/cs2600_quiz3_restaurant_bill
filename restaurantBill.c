9c9eac8fcbab983140bc26ba2a81b16cfb3ff836
Matthew Finerty <mfinerty@cpp.edu>
2022-10-23 15:04:21 -0700

    Create intial code outline

---
+++ restaurantBill.c
@@ -0,0 +1,15 @@
+// External table
+// extern Meal MealTable[];
+// extern const int MealTableEntries;
+
+int main(int argc, char const *argv[]) {
+    // Check argument count (should be >= 3)
+
+    // Parse command line arguments for tax and tip (convert to double)
+
+    // Randomly select a meal in the meal table
+
+    // Calculate meal cost, tax amount, tip amount, and total bill
+
+    // Display cost information to user
+}
